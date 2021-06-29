d579be8121a480171c20ccfe88207d679408c433
MalkonF <sudo777@protonmail.com>
2021-06-29 19:20:58 -0300

    Operações agregadoras

--- src/main/kotlin/me/malkon/arrays/IntroArrays.kt
+++ src/main/kotlin/me/malkon/arrays/IntroArrays.kt
@@ -1,30 +1,28 @@
 package me.malkon.arrays
 
 fun main() {
-    val serie: IntRange = 1.rangeTo(10)
-    for (s in serie) {
-        print("$s ")
-    }
+    val idades: IntArray = intArrayOf(10, 12, 18, 33, 40, 67)
+    val maiorIdade = idades.maxOrNull()
+    println(maiorIdade)
 
-    val numPar = 0..100 step 2
-    for (numeroPar in numPar) {
-        println("$numeroPar ")
-    }
+    val menorIdade = idades.minOrNull()
+    println(menorIdade)
 
-    val numerosParesReverso = 100 downTo 0 step 2
-    numerosParesReverso.forEach { print("${it} ") }
+    val media: Double = idades.average()
+    println(media)
 
-    val intervalo = 1500.0..5000.0
-    val salario = 4000.0
-    if (salario in intervalo) {
-        println("Esta no intervalo")
-    } else {
-        println("Não está dentro do intervalo")
-    }
+    var todosMaiores = idades.all { it > 18 } //retorna true se todos os elementos do array condizeram com a condição
+    println("Todos maiores? ${todosMaiores}")
 
-    val alfa = 'a'..'z'
-    val letra = 'k'
-    println(letra in alfa)
+    val existeMaior = idades.any { it >= 18 }
+    println("Algum aluno maior de idade? ${existeMaior}")
+
+    val maiores = idades.filter { it >= 18 } //retorna uma lista com todos valores maior de 18
+    println(maiores)
+
+    val busca = idades.find { it == 18 } //se tiver 2 valores iguais 18 ele retorna o primeiro
+    println(busca)
+
 }
 
 
