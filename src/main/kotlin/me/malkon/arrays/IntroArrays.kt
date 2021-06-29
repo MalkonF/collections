47b6778937dd84b3778dabd1c09be9fad47a846d
MalkonF <sudo777@protonmail.com>
2021-06-29 16:22:25 -0300

    Vendo algumas funções

--- src/main/kotlin/me/malkon/arrays/IntroArrays.kt
+++ src/main/kotlin/me/malkon/arrays/IntroArrays.kt
@@ -1,29 +1,8 @@
 package me.malkon.arrays
 
 fun main() {
-    val idade1 = 25
-    val idade2 = 19
-    val idade3 = 33
-    val idade4 = 20
-
-    val maiorIdade = if (idade1 > idade2 && idade1 > idade3 && idade1 > idade4) {
-        idade1
-    } else if (idade2 > idade3 && idade2 > idade4) {
-        idade2
-    } else if (idade3 > idade4) {
-        idade3
-    } else {
-        idade4
-    }
-
-    println(maiorIdade)
-    //com arrays fica mais organizado, mais fácil a manutenção e adição de novas idades
-    // e com desempenho melhor que os if e else acima
-    val idades = IntArray(4) //o tamanho do array n pode ser mudado em tempo de execução
-    idades[0] = 25
-    idades[1] = 19
-    idades[2] = 33
-    idades[3] = 20
+    //essa função tem o código mais conciso que array tradicional
+    val idades: IntArray = intArrayOf(25, 19, 33, 20, 55)
 
     var maiorIdade2 = 0
 
@@ -33,4 +12,14 @@ fun main() {
         }
     }
     println(maiorIdade2)
+
+    var menorIdade = Int.MAX_VALUE
+    //função forEach código fica mais organizado e fácil de manter. Desempenho maior?
+    idades.forEach { idade ->
+        if (idade < menorIdade) {
+            menorIdade = idade
+        }
+    }
+
+    println(menorIdade)
 }
