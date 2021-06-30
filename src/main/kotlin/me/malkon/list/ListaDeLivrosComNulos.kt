84fe6764074bf9588f8f0ae63904e7546f80bb25
MalkonF <sudo777@protonmail.com>
2021-06-30 12:21:35 -0300

    Lidando com atributos nulos / Elvis operator

--- src/main/kotlin/me/malkon/list/ListaDeLivrosComNulos.kt
+++ src/main/kotlin/me/malkon/list/ListaDeLivrosComNulos.kt
@@ -1,6 +1,9 @@
 package me.malkon.list
 
-fun main(){//para poder chamar essa função imprimeComMarcadores ela teve que ser modificada para aceitar valores nulos
-    //fun List<Livro?>.imprimeComMarcadores() {
-listaDeLivrosComNulos.imprimeComMarcadores()
+fun main() {
+    listaDeLivros
+        .groupBy { it.editora ?: "Editora desconhecida" } //vai agrupar livros pela editora. O elvis operator vai jogar esse valor qnd for nulo
+        .forEach { (editora, livros) ->//p cada elementos vai ter uma chave que é a editora e um valor que é o livr
+            println("$editora: ${livros.joinToString { it.titulo }}")//imprime a editora e o titulo livro
+        }
 }
