9faa99182af1bc3828849d87ff083ffb8cb11f74
MalkonF <sudo777@protonmail.com>
2021-06-30 15:57:18 -0300

    Organizando os livros através de listas imutáveis

--- src/main/kotlin/me/malkon/list/Prateleira.kt
+++ src/main/kotlin/me/malkon/list/Prateleira.kt
@@ -2,16 +2,16 @@ package me.malkon.list
 
 data class Prateleira(
     val genero: String,
-    val livros: MutableList<Livro>
+    val livros: List<Livro>
 
-){
-    fun organizarPorAutor(): MutableList<Livro>{
-        livros.sortBy { it.autor }
-        return livros
+) {
+    fun organizarPorAutor(): List<Livro> {
+        // livros.sortBy { it.autor }//sortBy nao pode ser usado com List pq ela é imutável e n pode ser alterada
+        return livros.sortedBy { it.autor }
+
     }
 
-    fun organizarPorAnoPublicacao(): MutableList<Livro> {
-        livros.sortBy { it.anoPublicacao }
-        return livros
+    fun organizarPorAnoPublicacao(): List<Livro> {
+        return livros.sortedBy { it.anoPublicacao }
     }
 }
