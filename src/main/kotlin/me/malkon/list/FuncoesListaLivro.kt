6674803addb91a6a60a84a7e9b780ed71131042d
MalkonF <sudo777@protonmail.com>
2021-06-30 12:03:25 -0300

    Filtrando valores nulos

--- src/main/kotlin/me/malkon/list/FuncoesListaLivro.kt
+++ src/main/kotlin/me/malkon/list/FuncoesListaLivro.kt
@@ -1,8 +1,11 @@
 package me.malkon.list
 
 fun List<Livro?>.imprimeComMarcadores() {
-    val joinToString = this.joinToString(separator = "\n") {
-        " - ${it?.titulo} de ${it?.autor}"//vai tentar chamar o titulo e autor, se for nulo, vai só mostrar o nulo ao inves de dar nullPointerException
-    }
+    val joinToString = this
+        .filterNotNull() //forma mais legível, faz a mesma coisa abaixo
+        //.filter { it != null } //só filtra valores diferentes de nulos
+        .joinToString(separator = "\n") {
+            " - ${it.titulo} de ${it.autor}"//vai tentar chamar o titulo e autor, se for nulo, vai só mostrar o nulo ao inves de dar nullPointerException
+        }
     println("#### Lista de livros #### \n$joinToString")
 }
